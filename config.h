3b6bb39b211752b57e227a16b8282e0430fdaad1
Chen-Hailin <chenhailin2@gmail.com>
2017-10-16 12:48:41 +0800

    migrate avx from 3.2.7 to 3.2.6

---
+++ config.h
@@ -0,0 +1,47 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.mrbayes.net"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "mrbayes 3.2"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "mrbayes"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mrbayes"
+
+/* Define to 1 if you have the `readline' library (-lreadline). */
+#define HAVE_LIBREADLINE 1
+
+/* Define to 1 if you have a recent version of readline */
+#define COMPLETIONMATCHES 1
+
+/* Define to 1 if you want to compile the parallel version */
+/* #undef MPI_ENABLED */
+
+/* Define to 1 if you want to compile SSE-enabled version */
+#define SSE_ENABLED 1
+
+/* Define to 1 to support Advanced Vector Extensions */
+#define HAVE_AVX 1
+
+/* Define one of the UNIX, MAC or WINDOWS */
+#define UNIX_VERSION 1
+/* #undef MAC_VERSION */
+/* #undef WINDOWS_VERSION */
+
+/* Define if used on a 64bit cpu */
+#define _64BIT 1
+
+/* Define to enable beagle library */
+#define BEAGLE_ENABLED 1
+
+/* Define to enable threads in BEAGLE library */
+/* #undef THREADS_ENABLED */
+#define OPENMP 0
+
+#define MPI_ENABLED 1
